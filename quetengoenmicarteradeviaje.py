0fa91a8a4eaa1ef0e66624d6785a1f68eb756dea
jr1hla3769 <jackrh240@gmail.com>
2025-02-01 10:18:53 +0000

    Add files via upload
    value of my travel wallet

---
+++ quetengoenmicarteradeviaje.py
@@ -0,0 +1,12 @@
+
+
+
+b_5 = int(input("billetes de 5£: "))
+b_10 = int(input("billetes de 10£: "))
+b_20 = int(input("billetes de 20£: "))
+b_50 = int(input("billetes de 50£: "))
+
+
+total = (b_5 * 5) + (b_10 * 10) + (b_20 * 20) + (b_50 * 50)
+
+print(total)
