2a0585fb522c87ecadfece9abdb80f301e4a2dea
jr1hla3769 <jackrh240@gmail.com>
2025-02-02 00:09:47 +0000

    Add files via upload

---
+++ budget calculator1.py
@@ -0,0 +1,7 @@
+venue_cost = 12000
+confirmed_guests = 50
+catering_cost = 250 * confirmed_guests
+entertainment_cost = 9000
+# calculate TTL
+total_cost = venue_cost + catering_cost + entertainment_cost
+print(f"Total Event Cost: £{total_cost}")
